9dcce7b7be022e71520e09e8a7233ae8090a7599
rayharli <50549120+rayharli@users.noreply.github.com>
2023-01-26 22:26:47 +0200

    Update script.js

--- example1/script.js
+++ example1/script.js
@@ -4,6 +4,7 @@ import * as THREE from 'three'
 // uncomment to load controls
 //import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
+
 // create a scene and a camera
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
